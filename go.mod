fec2de1b01db4615f1ddf941a6c8e1e92d4c6676
matteo107 <matteo107@gmail.com>
2019-02-27 12:37:56 +0200

    Initial commit. Application structure

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/matteo107/go-echo-vue
+
+go 1.12
+
+require (
+	github.com/labstack/echo/v4 v4.0.0
+	github.com/mattn/go-sqlite3 v1.10.0
+)
---
+++ todo.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	// Db
+	db := initDB("storage.db")
+	migrate(db)
+
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", func(c echo.Context) error { return c.JSON(200, "Hello") })
+
+	e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "GET Tasks") })
+	e.PUT("/tasks", func(c echo.Context) error { return c.JSON(200, "PUT Tasks") })
+	e.DELETE("/tasks/:id", func(c echo.Context) error { return c.JSON(200, "DELETE Task "+c.Param("id")) })
+	// Start server
+	e.Logger.Fatal(e.Start(":8000"))
+
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	// Here we check for any db errors then exit
+	if err != nil {
+		panic(err)
+	}
+
+	// If we don't get any errors but somehow still don't get a db connection
+	// we exit as well
+	if db == nil {
+		panic("db nil")
+	}
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `
+    CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL
+    );
+    `
+
+	_, err := db.Exec(sql)
+	// Exit if something goes wrong with our SQL statement above
+	if err != nil {
+		panic(err)
+	}
+}
